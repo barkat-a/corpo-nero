501fc6ea30622dddc0e2e58940e2ca22aded1bf8
barkat-a <barkat99@gmail.com>
2024-01-29 12:36:14 -0500

    recent notes

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -23,6 +23,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
+    Component.DesktopOnly(Component.RecentNotes()),
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
